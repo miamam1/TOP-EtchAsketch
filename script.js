ee0f584495e4147f968b0966e42c9d01324ef221
miamam1 <git@github.com:miamam1/git_test.git>
2023-08-03 17:22:18 +0100

    Test

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container')
+
+
+for(let i = 0; i < 2; i++) {
+    const gridSquare = document.createElement('div')
+    gridSquare.style.border = " dotted black"
+    container.appendChild(gridSquare)
+
+}
+
