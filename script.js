fb685e323a85102f9dc39736c94921ae6f7e66cd
miamam1 <git@github.com:miamam1/git_test.git>
2023-08-09 17:14:40 +0100

    completely stuck tbh guessing

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const container = document.querySelector('.container')
 
+//so idk what im doing i might need to redo this completely use google n shit
+//NVM IM A FUCKING JENIUS HOLY SHITTTIJPEAJEIJERIQJWPJPIEW
+//but it doesnt wotk really cus its not 16x16 its just.. 16 need to consider y axis as well 
+//this is probably a for loop nested inside a this for loop
+//logic is: for each div created, create 16 divs under that div 
 
-for(let i = 0; i < 2; i++) {
+for(let i = 0; i < 255; i++) {
     const gridSquare = document.createElement('div')
-    gridSquare.style.border = " dotted black"
+    gridSquare.style.border = " solid black"
+    gridSquare.style.width = "25px"
+    gridSquare.style.height = "12px"
+    gridSquare.style.background = "white"
+    gridSquare.style.border = "1px solid grey"
     container.appendChild(gridSquare)
-
+    
+    
 }
+
+
 
