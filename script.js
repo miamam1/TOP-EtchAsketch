c113a7fdf440773cdd1c31574a91ce49765c29b9
miamam1 <git@github.com:miamam1/git_test.git>
2023-08-15 14:03:34 +0100

    added basic functioanlity for now, added rainbow colours as well

--- script.js
+++ script.js
@@ -1,39 +1,48 @@
 const container = document.querySelector('.container')
  
- function createGrid(x) {
+ function createGrid(x, bgColor) {
     // so - 2 is needed to account for the space taken up by the border.
     // i think border takes up double the px: so eg. a border with 7px solid black 
     // would require another 14px to be accounte for, so calculation would be -14 instead
     // of 2
     let y = (600 / x) - 2
     x *= x
+    container.innerHTML = ''
     for(let i = 0; i < x; i++) {
         const gridSquare = document.createElement('div')
         gridSquare.style.border = "1px solid black"
         gridSquare.style.width = y + 'px'
         gridSquare.style.height = y + 'px'
-        gridSquare.style.background = "white"
-        
-        
-        
-        
-        
-        gridSquare.addEventListener('mouseenter' , () => gridSquare.style.background = "black" )
+        gridSquare.style.background = bgColor
+        gridSquare.addEventListener('mouseenter' , () => gridSquare.style.background = "#" +  Math.floor(Math.random()*16777215).toString(16) )
         container.appendChild(gridSquare)
         
     }
+
     
+    
+}
+
+
+
+createGrid(16, "yellow")
 
 
 
 
+function userInputtedGrid() {
+    x = prompt("What size would you like your container to be?")
+    y = prompt("What background color?")
+    createGrid(x, y)
+    
 }
 
 
 
 
 
-createGrid(4)
+
+
 
 
 
