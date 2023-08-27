633f71f399eca92cb8c6ceee9a4bb97f01586cac
miamam1 <git@github.com:miamam1/git_test.git>
2023-08-27 09:06:59 +0100

    New features in development: -Added buttons for functions such as new canvas, clear and rainbow effect

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container')
  
- function createGrid(x, bgColor) {
+ function createGrid(x, bgColor, penColor) {
     // so - 2 is needed to account for the space taken up by the border.
     // i think border takes up double the px: so eg. a border with 7px solid black 
     // would require another 14px to be accounte for, so calculation would be -14 instead
@@ -14,37 +14,89 @@ const container = document.querySelector('.container')
         gridSquare.style.width = y + 'px'
         gridSquare.style.height = y + 'px'
         gridSquare.style.background = bgColor
-        gridSquare.addEventListener('mouseenter' , () => gridSquare.style.background = "#" +  Math.floor(Math.random()*16777215).toString(16) )
+        gridSquare.addEventListener('mouseenter' , () => gridSquare.style.background = penColor)
         container.appendChild(gridSquare)
         
     }
+    
+    
+}
 
+function rainbow(x, bgColor) {
+    // so - 2 is needed to account for the space taken up by the border.
+    // i think border takes up double the px: so eg. a border with 7px solid black 
+    // would require another 14px to be accounte for, so calculation would be -14 instead
+    // of 2
+    let y = (600 / x) - 2
+    x *= x
+    container.innerHTML = ''
+    for(let i = 0; i < x; i++) {
+        const gridSquare = document.createElement('div')
+        gridSquare.style.border = "1px solid black"
+        gridSquare.style.width = y + 'px'
+        gridSquare.style.height = y + 'px'
+        gridSquare.style.background = bgColor
+        gridSquare.addEventListener('mouseenter' , () => gridSquare.style.background = "#" + Math.floor(Math.random()*16777215).toString(16))
+        container.appendChild(gridSquare)
+        
+    }
     
     
 }
 
 
 
-createGrid(16, "yellow")
 
 
 
+
+
+
+
+
+
+
+// Math.floor(Math.random()*16777215).toString(16)
+ createGrid(16, "white" , "black")
+
+
+let userSize = 16
+let userColor = "white"
+let userPenColor = "black"
 
 function userInputtedGrid() {
-    x = prompt("What size would you like your container to be?")
-    y = prompt("What background color?")
-    createGrid(x, y)
+    userSize = prompt("What size would you like your container to be?")
+    userColor = prompt("What background color?")
+    userPenColor = prompt("What pen color?")
+    createGrid(userSize, userColor, userPenColor)
+    
     
 }
 
 
 
+console.log(userSize)
+console.log(userColor)
+
+function clearGrid() {
+    if(rainbowActive == true) {
+        rainbow(userSize, userColor)
+    }
+    else{
+        createGrid(userSize, userColor, userPenColor)
+        
+    }
+    
+}
 
 
 
+let rainbowActive = false
 
-
-
+function rainbowBtn() {
+    rainbow(userSize, userColor)
+    return rainbowActive = true
+}
 
 
 
